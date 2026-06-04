54d33af600373f8bd17813575bf58a3a67c55e32
RominaPI <rominapavonibarra@gmail.com>
2026-06-04 16:52:55 -0600

    fix: cors production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,26 +3,21 @@ import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  try {
-    console.log('ANTES DE CREATE');
+  const app = await NestFactory.create(AppModule);
 
-    const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://precious-magic-production-9914.up.railway.app'
+    ],
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
 
-    console.log('DESPUES DE CREATE');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-    const port = 3000;
-
-    console.log('ANTES DE LISTEN');
-
-    await app.listen(port);
-
-    console.log('DESPUES DE LISTEN');
-
-    const logger = new Logger('Bootstrap');
-    logger.log(`Servidor en http://localhost:${port}`);
-  } catch (error) {
-    console.error('ERROR EN BOOTSTRAP:', error);
-  }
+  new Logger('Bootstrap').log(`Server running on ${port}`);
 }
 
 bootstrap();
